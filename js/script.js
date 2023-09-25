9b989cb13396f16311bae1dc0095e269ae760129
BGM DEVELOPMENT <121253933+BGM-DEVELOPMENT@users.noreply.github.com>
2023-09-25 19:32:28 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
-function scrollToElement(elementSelector, instance = 0) {
-    // Select all elements that match the given selector
-    const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the selector and if the requested instance exists
-    if (elements.length > instance) {
-        // Scroll to the specified instance of the element
-        elements[instance].scrollIntoView({ behavior: 'smooth' });
+function scrollToSection(element) {
+    const targetId = element.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+        const startPosition = window.pageYOffset;
+        const targetPosition = targetSection.offsetTop;
+        const distance = targetPosition - startPosition;
+        const duration = 1000;
+        const startTime = performance.now();
+
+        function animation(currentTime) {
+            const elapsedTime = currentTime - startTime;
+
+            const progress = Math.min(elapsedTime / duration, 1);
+            const newPosition = startPosition + distance * progress;
+
+            window.scrollTo(0, newPosition -75);
+
+            if (progress < 1) {
+                requestAnimationFrame(animation);
+            }
+        }
+
+        requestAnimationFrame(animation);
     }
 }
 
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-
-link1.addEventListener('click', () => {
-    scrollToElement('.header');
-});
-
-link2.addEventListener('click', () => {
-    scrollToElement('.header', 1);
+const scrollLinks = document.querySelectorAll('a[data-scroll="true"]');
+scrollLinks.forEach(function(link) {
+    link.addEventListener("click", function(e) {
+        e.preventDefault();
+        scrollToSection(link);
+    });
 });
